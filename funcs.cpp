2bf873de7f3d12e222ff87af1540ea339537f930
Emily Klapper <emiklap@gmail.com>
2022-12-12 08:41:07 -0500

    updated comment blocks

--- funcs.cpp
+++ funcs.cpp
@@ -4,6 +4,7 @@
 
 /**
  * Removes the leading spaces of a given line
+ * @param line the string that the function will remove the leading spaces from
  */
 std::string removeLeadingSpaces(std::string line) {
     std::string return_string;
@@ -24,6 +25,8 @@ std::string removeLeadingSpaces(std::string line) {
 
 /**
  * Counts the number of times a given charatcer is present in a given string
+ * @param line the string that the function will count the characers in
+ * @param c the character the function will count for
  */
 int countChar(std::string line, char c) {
     int counter = 0;
@@ -38,6 +41,8 @@ int countChar(std::string line, char c) {
 
 /**
  * Adds 1 or more tabs to the beginning of a given string depending on the number specified
+ * @param line the string the function will modify
+ * @param num_of_tabs the number of tabs that the functino will add to the string
  */
 std::string addTabs(std::string line, int num_of_tabs) {
     std::string tabs;
